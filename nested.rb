fdefccb2f14af5172aa8746b44e4e90946417250
Vincent Tran <vincentuong@yahoo.com>
2021-05-24 23:50:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -113,7 +113,10 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+programmer_hash[:alan_kay][:known_for] = {
+  
+}
+
 end
 
 def adding_to_dennis
