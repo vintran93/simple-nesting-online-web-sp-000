b51e0ca7a8b54aabad7799bf5b977e8482885178
Vincent Tran <vincentuong@yahoo.com>
2021-05-24 23:54:38 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
+  
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+ 
+programmer_hash[:dennis_ritchie][:languages][0]
+    
 end
 
 def adding_matz
@@ -79,8 +84,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:yukihiro_matsumoto] = {
+  known_for: "Ruby",
+  languages: ["LISP", "C"]
+}
 
-    
+programmer_hash
+
 end
 
 def changing_alan
@@ -102,7 +113,10 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+programmer_hash[:alan_kay][:known_for] = "GUI"
+
+programmer_hash
+  
 end
 
 def adding_to_dennis
@@ -121,5 +135,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+programmer_hash
 
 end
